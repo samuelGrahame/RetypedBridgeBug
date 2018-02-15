8022a55592d5f641d4837a96434bff1362f0a6ff
samuel grahame <samuel@intelogy.com.au>
2018-02-16 10:45:42 +1100

    Add project files.

---
+++ RetypedBridgeBug/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#if BRIDGE
+using static Retyped.jquery;
+#endif
+
+namespace RetypedBridgeBug
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DoSomething();
+        }
+        public static void DoSomething()
+        {
+#if BRIDGE
+            jQuery.ajax("Localhost/test.php");
+#endif
+        }
+    }
+}
